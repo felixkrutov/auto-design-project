ef7c234025bf789159f0861b01867b7c8cab66c1
felixkrutov <googoldecaplex@bk.ru>
2025-07-15 12:55:18 +0300

    Update main.py

--- generator_v2/main.py
+++ generator_v2/main.py
@@ -16,7 +16,6 @@ def run_generation_pipeline(project_file: str, output_file: str):
 
     print(f"2. Начинаем обработку проекта: '{project_data['meta']['project_name']}'")
     
-    # ИЗМЕНЕНИЕ ЗДЕСЬ: передаем все данные, а не по частям
     final_placements = calculate_placements(project_data)
     
     if not final_placements:
